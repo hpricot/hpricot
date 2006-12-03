779e6a1b91d508bd58b85f012370db0e26fcd42f
llebron <llebron@9e82565c-fa0a-db11-9fa0-00132028b6dc>
2006-12-03 16:24:00 +0000

    Removed file/folder
    git-svn-id: https://code.whytheluckystiff.net/svn/hpricot/trunk@81 9e82565c-fa0a-db11-9fa0-00132028b6dc

--- test/load_files.rb
+++
@@ -1,7 +0,0 @@
-module TestFiles
-    Dir.chdir(File.dirname(__FILE__)) do
-        Dir['files/*.{html,xhtml}'].each do |fname|
-            const_set fname[%r!/(\w+)\.\w+$!, 1].upcase, IO.read(fname)
-        end
-    end
-end
