26768fbcd6eef09284734652c3adab89a604f560
why <why@9e82565c-fa0a-db11-9fa0-00132028b6dc>
2006-12-04 16:52:39 +0000

    * test: Undo commits 78-82.
    git-svn-id: https://code.whytheluckystiff.net/svn/hpricot/trunk@83 9e82565c-fa0a-db11-9fa0-00132028b6dc

---
+++ test/test_parser.rb
@@ -0,0 +1,249 @@
+#!/usr/bin/env ruby
+
+require 'test/unit'
+require 'hpricot'
+require 'load_files'
+
+class TestParser < Test::Unit::TestCase
+  def test_set_attr
+    @basic = Hpricot.parse(TestFiles::BASIC)
+    @basic.search('//p').set('class', 'para')
+    assert_equal 4, @basic.search('//p').length
+    assert_equal 4, @basic.search('//p').find_all { |x| x['class'] == 'para' }.length
+  end
+
+  # Test creating a new element 
+  def test_new_element 
+    elem = Hpricot::Elem.new(Hpricot::STag.new('form')) 
+    assert_not_nil(elem) 
+    assert_not_nil(elem.attributes) 
+  end 
+
+  def test_scan_text
+    assert_equal 'FOO', Hpricot.make("FOO").first.content
+  end
+
+  def test_get_element_by_id
+    @basic = Hpricot.parse(TestFiles::BASIC)
+    assert_equal 'link1', @basic.get_element_by_id('link1')['id']
+    assert_equal 'link1', @basic.get_element_by_id('body1').get_element_by_id('link1').get_attribute('id')
+  end
+
+  def test_get_element_by_tag_name
+    @basic = Hpricot.parse(TestFiles::BASIC)
+    assert_equal 'link1', @basic.get_elements_by_tag_name('a')[0].get_attribute('id')
+    assert_equal 'link1', @basic.get_elements_by_tag_name('body')[0].get_element_by_id('link1').get_attribute('id')
+  end
+
+  def test_output_basic
+    @basic = Hpricot.parse(TestFiles::BASIC)
+    @basic2 = Hpricot.parse(@basic.inner_html)
+    scan_basic @basic2
+  end
+
+  def test_scan_basic
+    @basic = Hpricot.parse(TestFiles::BASIC)
+    scan_basic @basic
+  end
+
+  def scan_basic doc
+    assert_equal 'link1', doc.at('#link1')['id']
+    assert_equal 'link1', doc.at("p a")['id']
+    assert_equal 'link1', (doc/:p/:a).first['id']
+    assert_equal 'link1', doc.search('p').at('a').get_attribute('id')
+    assert_equal 'link2', (doc/'p').filter('.ohmy').search('a').first.get_attribute('id')
+    assert_equal (doc/'p')[2], (doc/'p').filter(':nth(2)')[0]
+    assert_equal (doc/'p')[2], (doc/'p').filter('[2]')[0]
+    assert_equal 4, (doc/'p').filter('*').length
+    assert_equal 4, (doc/'p').filter('* *').length
+    eles = (doc/'p').filter('.ohmy')
+    assert_equal 1, eles.length
+    assert_equal 'ohmy', eles.first.get_attribute('class')
+    assert_equal 3, (doc/'p:not(.ohmy)').length
+    assert_equal 3, (doc/'p').not('.ohmy').length
+    assert_equal 3, (doc/'p').not(eles.first).length
+    assert_equal 2, (doc/'p').filter('[@class]').length
+    assert_equal 'last final', (doc/'p[@class~="final"]').first.get_attribute('class')
+    assert_equal 1, (doc/'p').filter('[@class~="final"]').length
+    assert_equal 2, (doc/'p > a').length
+    assert_equal 1, (doc/'p.ohmy > a').length
+    assert_equal 2, (doc/'p / a').length
+    assert_equal 2, (doc/'link ~ link').length
+    assert_equal 3, (doc/'title ~ link').length
+  end
+
+  def test_scan_boingboing
+    @boingboing = Hpricot.parse(TestFiles::BOINGBOING)
+    assert_equal 60, (@boingboing/'p.posted').length
+    assert_equal 1, @boingboing.search("//a[@name='027906']").length
+  end
+
+  def test_reparent
+    doc = Hpricot(%{<div id="blurb_1"></div>})
+    div1 = doc.search('#blurb_1')
+    div1.before('<div id="blurb_0"></div>')
+
+    div0 = doc.search('#blurb_0')
+    div0.before('<div id="blurb_a"></div>')
+
+    assert_equal 'div', doc.at('#blurb_1').name
+  end
+
+  def test_siblings
+    @basic = Hpricot.parse(TestFiles::BASIC)
+    t = @basic.at(:title)
+    e = t.next_sibling
+    assert_equal 'test1.css', e['href']
+    assert_equal 'title', e.previous_sibling.name
+  end
+
+  def test_css_negation
+    @basic = Hpricot.parse(TestFiles::BASIC)
+    assert_equal 3, (@basic/'p:not(.final)').length
+  end
+
+  def test_remove_attribute
+    @basic = Hpricot.parse(TestFiles::BASIC)
+    (@basic/:p).each { |ele| ele.remove_attribute('class') }
+    assert_equal 0, (@basic/'p[@class]').length
+  end
+
+  def test_abs_xpath
+    @boingboing = Hpricot.parse(TestFiles::BOINGBOING)
+    assert_equal 60, @boingboing.search("/html/body//p[@class='posted']").length
+    assert_equal 60, @boingboing.search("/*/body//p[@class='posted']").length
+    assert_equal 18, @boingboing.search("//script").length
+    divs = @boingboing.search("//script/../div")
+    assert_equal 2,  divs.length
+    assert_equal 1,  divs.search('a').length
+    imgs = @boingboing.search('//div/p/a/img')
+    assert_equal 15, imgs.length
+    assert_equal 17, @boingboing.search('//div').search('p/a/img').length
+    assert imgs.all? { |x| x.name == 'img' }
+  end
+
+  def test_predicates
+    @boingboing = Hpricot.parse(TestFiles::BOINGBOING)
+    assert_equal 2, @boingboing.search('//link[@rel="alternate"]').length
+    p_imgs = @boingboing.search('//div/p[/a/img]')
+    assert_equal 15, p_imgs.length
+    assert p_imgs.all? { |x| x.name == 'p' }
+    p_imgs = @boingboing.search('//div/p[a/img]')
+    assert_equal 18, p_imgs.length
+    assert p_imgs.all? { |x| x.name == 'p' }
+    assert_equal 1, @boingboing.search('//input[@checked]').length
+  end
+
+  def test_alt_predicates
+    @boingboing = Hpricot.parse(TestFiles::BOINGBOING)
+    assert_equal 2, @boingboing.search('//table/tr:last').length
+
+    @basic = Hpricot.parse(TestFiles::BASIC)
+    assert_equal "<p>The third paragraph</p>",
+        @basic.search('p:eq(2)').to_html
+    assert_equal '<p class="last final"><b>THE FINAL PARAGRAPH</b></p>',
+        @basic.search('p:last').to_html
+    assert_equal 'last final', @basic.search('//p:last-of-type').first.get_attribute('class')
+  end
+
+  def test_many_paths
+    @boingboing = Hpricot.parse(TestFiles::BOINGBOING)
+    assert_equal 62, @boingboing.search('p.posted, link[@rel="alternate"]').length
+    assert_equal 20, @boingboing.search('//div/p[a/img]|//link[@rel="alternate"]').length
+  end
+
+  def test_stacked_search
+    @boingboing = Hpricot.parse(TestFiles::BOINGBOING)
+    assert_kind_of Hpricot::Elements, @boingboing.search('//div/p').search('a img')
+  end
+
+  def test_class_search
+    # test case sent by Chih-Chao Lam
+    doc = Hpricot("<div class=xyz'>abc</div>")
+    assert_equal 1, doc.search(".xyz").length
+    doc = Hpricot("<div class=xyz>abc</div><div class=abc>xyz</div>")
+    assert_equal 1, doc.search(".xyz").length
+    assert_equal 2, doc.search("*").length
+  end
+
+  def test_kleene_star
+    # bug noticed by raja bhatia
+    doc = Hpricot("<span class='small'>1</span><div class='large'>2</div><div class='small'>3</div><span class='blue large'>4</span>")
+    assert_equal 2, doc.search("*[@class*='small']").length
+    assert_equal 2, doc.search("*.small").length
+    assert_equal 2, doc.search(".small").length
+    assert_equal 2, doc.search(".large").length
+  end
+
+  def test_body_newlines
+    @immob = Hpricot.parse(TestFiles::IMMOB)
+    body = @immob.at(:body)
+    {'background' => '', 'bgcolor' => '#ffffff', 'text' => '#000000', 'marginheight' => '10',
+     'marginwidth' => '10', 'leftmargin' => '10', 'topmargin' => '10', 'link' => '#000066',
+     'alink' => '#ff6600', 'hlink' => "#ff6600", 'vlink' => "#000000"}.each do |k, v|
+        assert_equal v, body[k]
+    end
+  end
+
+  def test_nested_twins
+    @doc = Hpricot("<div>Hi<div>there</div></div>")
+    assert_equal 1, (@doc/"div div").length
+  end
+
+  def test_wildcard
+    @basic = Hpricot.parse(TestFiles::BASIC)
+    assert_equal 3, (@basic/"*[@id]").length
+    assert_equal 3, (@basic/"//*[@id]").length
+  end
+
+  def test_javascripts
+    @immob = Hpricot.parse(TestFiles::IMMOB)
+    assert_equal 3, (@immob/:script)[0].inner_html.scan(/<LINK/).length
+  end
+
+  def test_nested_scripts
+    @week9 = Hpricot.parse(TestFiles::WEEK9)
+    assert_equal 14, (@week9/"a").find_all { |x| x.inner_html.include? "GameCenter" }.length
+  end
+
+  def test_uswebgen
+    @uswebgen = Hpricot.parse(TestFiles::USWEBGEN)
+    # sent by brent beardsley, hpricot 0.3 had problems with all the links.
+    assert_equal 67, (@uswebgen/:a).length
+  end
+
+  def test_mangled_tags
+    [%{<html><form name='loginForm' method='post' action='/units/a/login/1,13088,779-1,00.html'?URL=></form></html>},
+     %{<html><form name='loginForm' ?URL= method='post' action='/units/a/login/1,13088,779-1,00.html'></form></html>},
+     %{<html><form name='loginForm'?URL= ?URL= method='post' action='/units/a/login/1,13088,779-1,00.html'?URL=></form></html>},
+     %{<html><form name='loginForm' method='post' action='/units/a/login/1,13088,779-1,00.html' ?URL=></form></html>}].
+    each do |str|
+      doc = Hpricot(str)
+      assert_equal 1, (doc/:form).length
+      assert_equal '/units/a/login/1,13088,779-1,00.html', doc.at("form")['action']
+    end
+  end
+
+  def test_procins
+    doc = Hpricot("<?php print('hello') ?>\n<?xml blah='blah'?>")
+    assert_equal "php", doc.children[0].target
+    assert_equal "blah='blah'", doc.children[2].content
+  end
+
+  def test_buffer_error
+    assert_raise Hpricot::ParseError, "ran out of buffer space on element <input>, starting on line 3." do
+      Hpricot(%{<p>\n\n<input type="hidden" name="__VIEWSTATE"  value="#{(("X" * 2000) + "\n") * 22}" />\n\n</p>})
+    end
+  end
+
+  def test_filters
+    @basic = Hpricot.parse(TestFiles::BASIC)
+    assert_equal 0, (@basic/"title:parent").size
+    assert_equal 3, (@basic/"p:parent").size
+    assert_equal 1, (@basic/"title:empty").size
+    assert_equal 1, (@basic/"p:empty").size
+  end
+
+  def test_unicode
+  end
+end
